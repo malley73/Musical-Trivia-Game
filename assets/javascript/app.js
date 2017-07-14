8bcc7dc6cf2ab2673c92f0e0971fd653f09c2086
Michael Alley <michael.j.alley@gmail.com>
2017-07-14 18:13:28 -0400

    Still cant get java script to load

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,40 +1,43 @@
-  $(document).ready(function() {
-    console.log("ready!");
+$(document).ready(function() {
+  console.log("ready!");
 
-    // variables
-    var answer = 0;
+  // variables
+  var answer = 0;
 
 
-    // Application launcher
-    $(".start").click(function() {
-      start();
-    });
+  // Application launcher
+  $(".start").click(function() {
+    start();
+  });
 
-    // Application Functions
-    function start() {
-      var counter = 30;
-      var myVar = setInterval(function() { myTimer() }, 1000);
+  // Application Functions
+  function start() {
+    var counter = 30;
+    var myVar = setInterval(function() { myTimer() }, 1000);
 
-      function myTimer() {
-        counter--;
-        if (counter < 10) {
-          counter = "0" + String(counter);
-        }
-        $('#timerDisplay').html(":" + counter);
+    function myTimer() {
+      counter--;
+      if (counter < 10) {
+        counter = "0" + String(counter);
       }
-
-      $('#a1').click(function() {
-        answer = 1;
-      });
-      $('#a1').click(function() {
-        answer = 1;
-      });
-      $('#a1').click(function() {
-        answer = 1;
-      });
-      $('#a1').click(function() {
-        answer = 1;
-      });
+      $('#timerDisplay').html(":" + counter);
     }
 
-  });
+    $('#a1').click(function() {
+      answer = 1;
+    });
+    $('#a2').click(function() {
+      answer = 2;
+    });
+    $('#a3').click(function() {
+      answer = 3;
+    });
+    $('#a4').click(function() {
+      answer = 4;
+    });
+        $('#a5').click(function() {
+      answer = 5;
+    });
+  }
+
+});
